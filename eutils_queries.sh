02f3b7a1ca6f464814ab1ca65bc18b93ec8cd561
gmchen <gregorymchen@gmail.com>
2016-11-19 01:17:45 -0500

    Added keywords, xml output

--- eutils_queries.sh
+++ eutils_queries.sh
@@ -1,18 +1,20 @@
 #!/bin/bash
-OUTDIR="AbstractOutput"
+OUTDIR="Pubmed_JAMA"
 DATE=`date +%Y-%m-%d`
 mkdir "$OUTDIR"
 echo "Pubmed access date: $DATE\n\n" > "$OUTDIR/out.log"
 cat eutils_queries.sh >> "$OUTDIR/out.log"
-for YEAR in {2015..2014}
+for YEAR in {2015..1976}
 do
 	for MONTH in {1..12}
 	do
 		echo "Year $YEAR Month $MONTH"
 		esearch -db pubmed -query "JAMA [journal] AND $YEAR/$MONTH [dp]" | \
-		tee >(efetch -format xml | xtract -pattern Article -element ArticleTitle > "$OUTDIR/${YEAR}_${MONTH}_TitleText.txt") | \
-		efetch -format xml | xtract -pattern Abstract -element AbstractText > \
-		"$OUTDIR/${YEAR}_${MONTH}_AbstractText.txt"
+		efetch -format xml | \
+		tee >(xtract -pattern Article -element ArticleTitle > "$OUTDIR/${YEAR}_${MONTH}_TitleText.txt") | \
+		tee >(xtract -pattern MeshHeading -element DescriptorName > "$OUTDIR/${YEAR}_${MONTH}_KeywordText.txt") | \
+		tee >(xtract -pattern Abstract -element AbstractText > "$OUTDIR/${YEAR}_${MONTH}_AbstractText.txt") | \
+		> "$OUTDIR/xml/${YEAR}_${MONTH}.xml"
 		
 		sleep 5
 	done
