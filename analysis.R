998e130ee842f4caafb54d85fe8b6ce07620f93c
gmchen <gregorymchen@gmail.com>
2016-11-20 14:50:49 -0500

    Restructured code for loading titles, abstracts, or keywords

--- analysis.R
+++ analysis.R
@@ -1,65 +1,71 @@
 library(tm)
 library(SnowballC)
 
-titleTexts <- c()
-abstractTexts <- c()
-keywordTexts <- c()
+jamaTexts <- list(title=NULL, abstract=NULL, keyword=NULL)
 
 # Load data
 
 for(year in 1976:2015) {
   yearIndex <- year - 1975
-  #titleTexts[[yearIndex]] <- list()
-  #abstractTexts[[yearIndex]] <- list()
-  #keywordTexts[[yearIndex]] <- list()
   
   for(month in 1:12) {
-    #titleTexts[[yearIndex]][[month]] <- Corpus(DirSource("Pubmed_JAMA/", pattern = paste0(year, "_", month, "_KeywordText\\.txt")))
-    toAdd <- Corpus(DirSource("Pubmed_JAMA/", pattern = paste0(year, "_", month, "_TitleText\\.txt")))
-    meta(toAdd, "Year", "local") <- year
-    meta(toAdd, "Month", "local") <- month
-    meta(toAdd, "MonthIndex", "local") <- (year - 1976) * 12 + (month - 1) + 1
-    if(length(titleTexts) == 0) {
-      titleTexts <- toAdd
-    } else {
-      titleTexts <- c(titleTexts, toAdd)
+    # textType: 1 for title, 2 for abstract, 3 for keyword
+    for(textType in 1:3) {
+      if(textType == 1) {
+        toAdd <- Corpus(DirSource("Pubmed_JAMA/", pattern = paste0(year, "_", month, "_TitleText\\.txt")))
+      } else if (textType == 2) {
+        toAdd <- Corpus(DirSource("Pubmed_JAMA/", pattern = paste0(year, "_", month, "_AbstractText\\.txt")))
+      } else if(textType == 3) {
+        toAdd <- Corpus(DirSource("Pubmed_JAMA/", pattern = paste0(year, "_", month, "_KeywordText\\.txt")))  
+      }
+      meta(toAdd, "Year", "local") <- year
+      meta(toAdd, "Month", "local") <- month
+      meta(toAdd, "MonthIndex", "local") <- (year - 1976) * 12 + (month - 1) + 1
+      
+      if(is.null(jamaTexts[[textType]])) {
+        jamaTexts[[textType]] <- toAdd
+      } else {
+        jamaTexts[[textType]] <- c(jamaTexts[[textType]], toAdd)
+      }
     }
   }
 }
 
 # Stemming
-titleTexts <- tm_map(titleTexts, stemDocument)
+jamaTexts <- lapply(jamaTexts, function(currentCorpus) tm_map(currentCorpus, stemDocument))
 
 # To lowercase
-titleTexts <- tm_map(titleTexts, content_transformer(tolower))
+jamaTexts <- lapply(jamaTexts, function(currentCorpus) tm_map(currentCorpus, content_transformer(tolower)))
 
 # Remove specific words
 titleRemoveList <- c("letter: ", "editorial: ")
-for(i in 1:length(titleTexts)) {
+for(i in 1:length(jamaTexts$title)) {
   for(wordToRemove in titleRemoveList) {
-    titleTexts[[i]]$content <- gsub(wordToRemove, "", titleTexts[[i]]$content, fixed=TRUE)
+    jamaTexts$title[[i]]$content <- gsub(wordToRemove, "", jamaTexts$title[[i]]$content, fixed=TRUE)
   }
 }
 
 # Remove stopwords
-titleTexts <- tm_map(titleTexts, removeWords, stopwords("english"))
+jamaTexts <- lapply(jamaTexts, function(currentCorpus) tm_map(currentCorpus, removeWords, stopwords("english")))
 
 # Strip whitespace
-titleTexts <- tm_map(titleTexts, stripWhitespace)
+jamaTexts <- lapply(jamaTexts, function(currentCorpus) tm_map(currentCorpus, stripWhitespace))
 
 # Remove punctuation
-titleTexts <- tm_map(titleTexts, removePunctuation)
+jamaTexts <- lapply(jamaTexts, function(currentCorpus) tm_map(currentCorpus, removePunctuation))
 
 # Term-document matrix
 myMat <- DocumentTermMatrix(titleTexts)
+
+myMat <- DocumentTermMatrix(jamaTexts$title)
 matrix.colsums <- colSums(inspect(myMat))
 matrix.rowsums <- rowSums(inspect(myMat))
-freq.table <- data.frame(word=colnames(myMat)[rev(order(matrix.colsums))], count=rev(sort(matrix.colsums)))
+freq.count.table <- data.frame(word=colnames(myMat), count=matrix.colsums)
 
 myMat.freq.per.1000 <- t(t(inspect(myMat)) * 1000 / matrix.rowsums)
 
-# only include terms that have occurred at least 1% of the time
-myTDMHiFreq <- myMat.freq.per.1000[,matrix.colsums >= 100]
+# only include terms that have occurred at least 1/1000 (0.01%) of the time
+myTDMHiFreq <- myMat.freq.per.1000[,freq.count.table$count > 0.0001 * sum(freq.count.table$count)]
 
 # Go by year
 my.new.tdm <- matrix(nrow=40, ncol=ncol(myTDMHiFreq))
@@ -72,7 +78,13 @@ colnames(my.new.tdm) <- colnames(myTDMHiFreq)
 
 year.coefficients <- apply(my.new.tdm, 2, function(y) coef(lm(y ~ years))["years"])
 year.pvals <- apply(my.new.tdm, 2, function(y) summary(lm(y ~ years))$coefficients[2,4])
-p.adjust(year.pvals, method="fdr")
+
+fdr.adj.year.pvals <- p.adjust(year.pvals, method="fdr")
+
+sig.year.coefficients <- year.coefficients[fdr.adj.year.pvals < 0.01]
 
 barplot(my.new.tdm[,"patient"])
 
+# find all words after "among"
+out <- sapply(jamaTexts$title, function(x) unlist(str_extract_all(x$content, '(?<=among\\s)\\w+')))
+# Can do some analyses on just these words, can do fancy things like take up to the first noun after "among"
